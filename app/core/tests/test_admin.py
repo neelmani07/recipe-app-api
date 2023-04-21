4c8b4b5c654297a0d2f2d8b2d2afef227674cfbd
neelmani07 <manineel25@gmail.com>
2023-04-21 11:59:43 +0530

    added API Documrntationand swagger

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.test import Client
 
 
+
 class AdminSiteTests(TestCase):
     """Test for Django admin"""
 
@@ -38,6 +39,12 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code,200)
 
 
 
