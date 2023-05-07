a7110a8ec65bec63a7a45e51e238568f4fbbbb4f
neelmani07 <manineel25@gmail.com>
2023-05-07 21:02:28 +0530

    Recipes image dependencies

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -383,30 +383,30 @@ class ImageuploadTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_mode().objects.create_user(
+        self.user = get_user_model().objects.create_user(
             'user@example.com',
             'password123',
         )
-        self.client.force_authentication(self.user)
+        self.client.force_authenticate(self.user)
         self.recipe = create_recipe(user=self.user)
 
     def tearDown(self):
-        self.recipe.image.deleted()
+        self.recipe.image.delete()
 
-    def testUploadImage(self):
+    def test_upload_image(self):
         """Test uploading an image to a recipe."""
         url = image_upload_url(self.recipe.id)
         with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
             img = Image.new('RGB', (10,10))
             img.save(image_file, format='JPEG')
             image_file.seek(0)
-            payload = {'import': image_file}
+            payload = {'image': image_file}
             res = self.client.post(url, payload, format='multipart')
 
         self.recipe.refresh_from_db()
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn('image', res.data)
-        self.assertTrue(os.path.exist(self.recipe.image.path))
+        self.assertTrue(os.path.exists(self.recipe.image.path))
 
     def test_upload_image_bad_request(self):
         """Test uploading invalid image."""
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -42,17 +42,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-    @action(method=['POST'], detail=True, url_path='upload-image')
+    @action(methods=['POST'], detail=True, url_path='upload-image')
     def upload_image(self, request, pk=None):
         """Upload an image to recipe."""
         recipe = self.get_object()
-        serialzer = self.get_serializer(recipe, data=request.data)
+        serializer = self.get_serializer(recipe, data=request.data)
 
         if serializer.is_valid():
-            serialzer.save()
-            return Response(serialzer.data, status=status.HTTP_200_OK)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
